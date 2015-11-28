99b0cb2cbbda9a0fed60f080f3b4b2ef9fbc6cb6
Gary Jones <gary@gamajo.com>
2015-11-28 16:57:33 +0000

    Code Standards: first pass at PSR-2
    First pass at automated adherence to PSR-2 code standards.

--- EasyWsdl2PHP.php
+++ EasyWsdl2PHP.php
@@ -1,96 +1,74 @@
 <?php
-    class EasyWsdl2PHP
+
+class EasyWsdl2PHP
+{
+    static public function generate($url, $sname)
     {
-
-        static public function generate($url,$sname)
-        {
-            $soapClient       = new SoapClient($url);
-            $classesArr = array();
-
-            $functions = $soapClient->__getFunctions();
-
-            $nl="\n";
-
-
-            $code ='';
-            $simpletypes = array('string','int','double','dateTime','float');
-            foreach($functions as $func)
-            {
-                $temp = split(' ' ,$func,2);
-                //less process whateever is inside ()
-                $start = strpos($temp[1],'(');
-                $end = strpos($temp[1],'(');
-                $parameters = substr($temp[1],$start,$end);
-
-
-                $t1 = str_replace(')','',$temp[1]);
-                $t1 = str_replace('(',':',$t1);
-                $t2 = split(':',$t1);
-                $func = $t2[0];
-                $par = $t2[1];
-
-                $params = split(' ', $par);
-                $p1 = '$' . $params[0];
-
-
-                $code .= $nl .'function ' . $func . '(' . $p1 .')'
-                . "{$nl}{\n";
-                if ($temp[0] == 'void')
-                    $code .=  $nl ."\$this->soapClient->$func({$p1});{$nl}}";
-                else
-                {
-                    $code .=  $nl . '$' . $temp[0] . ' = ' .  "\$this->soapClient->$func({$p1});";
-                    $code .= $nl ."return \${$temp[0]};\n{$nl}}";
-                }
-
-
+        $soapClient = new SoapClient($url);
+        $classesArr = [];
+        $functions = $soapClient->__getFunctions();
+        $nl = "\n";
+        $code        = '';
+        $simpletypes = ['string', 'int', 'double', 'dateTime', 'float'];
+        foreach ($functions as $func) {
+            $temp = split(' ', $func, 2);
+            //less process whateever is inside ()
+            $start      = strpos($temp[1], '(');
+            $end        = strpos($temp[1], '(');
+            $parameters = substr($temp[1], $start, $end);
+            $t1   = str_replace(')', '', $temp[1]);
+            $t1   = str_replace('(', ':', $t1);
+            $t2   = split(':', $t1);
+            $func = $t2[0];
+            $par  = $t2[1];
+            $params = split(' ', $par);
+            $p1     = '$' . $params[0];
+            $code .= $nl . 'function ' . $func . '(' . $p1 . ')'
+                     . "{$nl}{\n";
+            if ($temp[0] == 'void') {
+                $code .= $nl . "\$this->soapClient->$func({$p1});{$nl}}";
+            } else {
+                $code .= $nl . '$' . $temp[0] . ' = ' . "\$this->soapClient->$func({$p1});";
+                $code .= $nl . "return \${$temp[0]};\n{$nl}}";
             }
-            $code .= "}\n{$nl}";
-
-
-            //    print_r($functions);
-            //    echo "<hr>";
-            $types = $soapClient->__getTypes();
-            // print_r($types);
-            $codeType ='';
-            foreach ($types as $type)
-            {
-                if (substr($type,0,6) == 'struct')
-                {
-                    $data = trim(str_replace(array('{','}'),'',substr($type,strpos($type, '{')+1)));
-                    $data_members = split(';',$data);
-                    //print_r($data_members);
-                    // echo "[" . $data . "]";
-                    $classname = trim(substr($type,6,strpos($type,'{')-6));
-
-                    //write object
-                    $codeType .= $nl . 'class ' . $classname .'{';
-                    $classesArr [] = $classname;
-                    foreach($data_members as $member)
-                    {
-                        $member = trim($member);
-                        if (strlen($member)< 1) continue;
-                        list($data_type,$member_name) = split(' ' , $member);
-                        $codeType .= "{$nl}var \${$member_name};//{$data_type}";
+        }
+        $code .= "}\n{$nl}";
+        //    print_r($functions);
+        //    echo "<hr>";
+        $types = $soapClient->__getTypes();
+        // print_r($types);
+        $codeType = '';
+        foreach ($types as $type) {
+            if (substr($type, 0, 6) == 'struct') {
+                $data         = trim(str_replace(['{', '}'], '', substr($type, strpos($type, '{') + 1)));
+                $data_members = split(';', $data);
+                //print_r($data_members);
+                // echo "[" . $data . "]";
+                $classname = trim(substr($type, 6, strpos($type, '{') - 6));
+                //write object
+                $codeType .= $nl . 'class ' . $classname . '{';
+                $classesArr [] = $classname;
+                foreach ($data_members as $member) {
+                    $member = trim($member);
+                    if (strlen($member) < 1) {
+                        continue;
                     }
-
-                    $codeType .= $nl . '}';
-
+                    list($data_type, $member_name) = split(' ', $member);
+                    $codeType .= "{$nl}var \${$member_name};//{$data_type}";
                 }
+                $codeType .= $nl . '}';
             }
-
-            $mapstr = "\n" . 'private static $classmap = array(';
-            $classMAPCode = array();
-            foreach($classesArr as $cname)
-            {
-                // $mapstr .= "\n,'$cname'=>'$cname'";
-                $classMAPCode[] = "'$cname'=>'$cname'\n";
-            }
-            //print_r($classMAPCode);
-            $mapstr .= implode (',',$classMAPCode);
-            $mapstr .= "\n);";
-
-            $fullcode = <<< EOT
+        }
+        $mapstr       = "\n" . 'private static $classmap = array(';
+        $classMAPCode = [];
+        foreach ($classesArr as $cname) {
+            // $mapstr .= "\n,'$cname'=>'$cname'";
+            $classMAPCode[] = "'$cname'=>'$cname'\n";
+        }
+        //print_r($classMAPCode);
+        $mapstr .= implode(',', $classMAPCode);
+        $mapstr .= "\n);";
+        $fullcode = <<< EOT
 <?php
 $codeType
 class $sname $nl {
@@ -105,8 +83,8 @@ class $sname $nl {
 ?>
 EOT;
 
-            return $fullcode;
-        }
-
+        return $fullcode;
     }
+}
+
 ?>
